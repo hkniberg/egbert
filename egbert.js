33c4aec8a5fd885d2e94162005d9cfc2a047b108
Henrik <h@kniberg.com>
2023-04-19 21:13:44 +0200

    You can also use the console to talk to egbert now

--- egbert.js
+++ egbert.js
@@ -7,27 +7,31 @@ const client = new Client({ intents: [
         GatewayIntentBits.MessageContent
     ] });
 
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-    console.log("Channels: " + c.channels.cache.size);
+    console.log(`Ready! Logged in as ${c.user.tag}. I see ${c.channels.cache.size} channels.`);
 });
 
 client.on(Events.MessageCreate, msg => {
-    console.log("Message created: " + msg.content);
-    if (msg.content.toLowerCase() === 'hi egbert') {
-        msg.reply('Shutup ' + msg.author.username);
+    const messageContent = msg.content;
+    console.log("Message created: " + messageContent);
+    maybeRespond(messageContent, msg.author.username, response => {
+        msg.reply(response);
+    });
+});
+
+function maybeRespond(message, author, callback) {
+    if (message.toLowerCase() === 'hi egbert') {
+        callback('Shutup ' + author);
     }
-});
-
-
-client.on(Events.InteractionCreate, async interaction => {
-    console.log("Interaction create!");
-});
+}
 
 
 // Log in to Discord with your client's token
 client.login(process.env.CLIENT_TOKEN);
 
+const stdin = process.openStdin();
+stdin.addListener("data", function(message) {
+    maybeRespond(message.toString().trim(), "Console user", response => {
+        console.log(response);
+    });
+});
