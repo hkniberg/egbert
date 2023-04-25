c7fad07f0b48e38594c65c9cb4fb6366ce325a28
Henrik <h@kniberg.com>
2023-04-25 18:02:03 +0200

    Minor cleanup of main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
-require('dotenv').config(); // Initialize dotenv
-const { maybeRespond } = require('./chat-logic'); // Import maybeRespond from chat-logic.ts
+require('dotenv').config();
+const { maybeRespond } = require('./chat-logic');
 const discordChatSource = require('./discord-chat-source');
 const utils = require('./utils');
-const defaultPersonality = utils.readRequiredConfigProperty('DEFAULT_PERSONALITY');
-
+const minecraft = require('./minecraft-chat-source');
 require('axios');
 
-discordChatSource.login(process.env.DISCORD_CLIENT_TOKEN, defaultPersonality).catch((error : unknown) => {
-    console.error(`Error logging in to Discord: ${error}. Will ignore Discord.`);
-});
+const defaultPersonality = utils.readRequiredConfigProperty('DEFAULT_PERSONALITY');
 
-// This is for testing in the console
+discordChatSource
+    .login(process.env.DISCORD_CLIENT_TOKEN, defaultPersonality)
+    .catch((error: any) => {
+        console.error(`Error logging in to Discord: ${error}. Will ignore Discord.`);
+    });
+
 const stdin = process.openStdin();
-stdin.addListener('data', async(message) => {
-    const response = await maybeRespond(message.toString().trim(), 'Console user','Console', defaultPersonality);
+stdin.addListener('data', async (message) => {
+    const response = await maybeRespond(message.toString().trim(), 'Console user', 'Console', defaultPersonality);
     console.log(response);
 });
 
-const minecraft = require('./minecraft-chat-source');
-minecraft.startWatchingLogFile(process.env.MINECRAFT_LOG_PATH,
-    process.env.MINECRAFT_MEMORY_SERVER_NAME, process.env.MINECRAFT_RCON_HOST, process.env.MINECRAFT_RCON_PORT, process.env.MINECRAFT_RCON_PASSWORD,
+minecraft.startWatchingLogFile(
+    process.env.MINECRAFT_LOG_PATH,
+    process.env.MINECRAFT_MEMORY_SERVER_NAME,
+    process.env.MINECRAFT_RCON_HOST,
+    process.env.MINECRAFT_RCON_PORT,
+    process.env.MINECRAFT_RCON_PASSWORD,
     defaultPersonality
 );
 
