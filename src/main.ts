de5719c7c9b0fef0a627a373d718daa2793f3c59
Marten Stenius <marten@stenius.org>
2023-05-03 18:05:23 +0200

    Fixed startup log message about social contexts.

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ for (const botConfig of config.bots) {
     for (const chatSource of chatSources.values()) {
         if (bot.isMemberOfAnySocialContext(chatSource.getSocialContexts())) {
             chatSource.addBot(bot);
-            console.log(`  Added bot '${bot.getName()}' to chat source '${chatSource.getName()}', since both are members of social context '${chatSource.getDefaultSocialContext()}'`);
+            console.log(`  Added bot '${bot.getName()}' to chat source '${chatSource.getName()}', since they share at least one social context.`);
         }
     }
 }
