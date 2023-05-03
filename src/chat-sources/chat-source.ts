dbadcae0dae58b895a6bb32e8548c4cbc66cd5c3
Henrik Kniberg <h@kniberg.com>
2023-05-03 10:47:43 +0200

    Added some docs to ChatSource

--- src/chat-sources/chat-source.ts
+++ src/chat-sources/chat-source.ts
@@ -1,5 +1,12 @@
 import {Bot} from "../bot";
-import {noEmptyString} from "../utils";
+import {noEmptyString} from "../util/utils";
+
+/**
+ * A ChatSource is a source of chat messages. For example Discord server, a Minecraft server, or a console.
+ * When started it will connect to a server (the details vary depending on which type of chat source),
+ * listen for messages, asks bots to generate responses, and send the response(s) back.
+ * All bots in the same social context are given a chance to generate a response.
+ */
 export abstract class ChatSource {
     protected readonly name: string;
     // will never be empty string
